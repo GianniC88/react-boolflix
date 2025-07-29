d4545d8be717a20b5c89b55bfaa77a62542411a1
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-29 11:32:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <header>header </header>
+      <main>main</main>
+      <footer>footer</footer>
+
+    </>
+  )
+}
+
+export default App
