50896b6f6f40cf51af291341d0ff06d04423418c
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-30 16:04:07 +0200

    Ristrutturazione della visualizzazione dei film nel componente Post: aggiunta di una sezione con layout a griglia e rimozione di codice non necessario.

--- src/contexts/Post.jsx
+++ src/contexts/Post.jsx
@@ -12,46 +12,58 @@ const languageToCountry = {
 export function Post({ movies, loading }) {
 	return (
 		<>
-			<ul>
-				{movies.map((movie) => {
-					const stelle = Math.round(movie.vote_average / 2);
-					return (
-						<li key={movie.id}>
-							<h2>{movie.title}</h2>
-							{/* Mostra il poster se disponibile */}
-							{movie.poster_path ? (
-								<img
-									src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-									alt={movie.title}
-									style={{ width: "150px", borderRadius: "8px" }}
-								/>
-							) : (
-								<div style={{ width: "150px", height: "225px", background: "#eee", display: "flex", alignItems: "center", justifyContent: "center" }}>
-									<span>Nessuna immagine</span>
+			<section className="bg-secondary p-">
+				<div className="container">
+					<div className="row">
+						{movies.map((movie) => {
+							const stelle = Math.round(movie.vote_average / 2);
+							return (
+								<div className="col-4 mb-4 mt-4 " key={movie.id}>
+									<div className="card ">
+										<div className="card-body  ">
+											<ul>
+												<li >
+													<h2>{movie.title}</h2>
+													{/* Mostra il poster se disponibile */}
+													{movie.poster_path ? (
+														<img
+															src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+															alt={movie.title}
+															style={{ width: "150px", borderRadius: "8px" }}
+														/>
+													) : (
+														<div style={{ width: "150px", height: "225px", background: "#eee", display: "flex", alignItems: "center", justifyContent: "center" }}>
+															<span>Nessuna immagine</span>
+														</div>
+													)}
+													<p><strong>Titolo originale:</strong> {movie.original_title}</p>
+													<span>
+														<strong>Lingua:</strong> {movie.original_language}{" "}
+														<Flag code={languageToCountry[movie.original_language] || "UN"} height="16" />
+													</span>
+													<p>
+														<strong>Voto:</strong> {movie.vote_average}
+														<span>
+															{/* Stelle piene */}
+															{Array.from({ length: stelle }, (_, i) => (
+																<span key={i} style={{ color: "#FFD700" }}>★</span>
+															))}
+															{/* Stelle vuote */}
+															{Array.from({ length: 5 - stelle }, (_, i) => (
+																<span key={i} style={{ color: "#ccc" }}>☆</span>
+															))}
+														</span>
+													</p>
+												</li>
+											</ul>
+										</div>
+									</div>
 								</div>
-							)}
-							<p><strong>Titolo originale:</strong> {movie.original_title}</p>
-							<span>
-								<strong>Lingua:</strong> {movie.original_language}{" "}
-								<Flag code={languageToCountry[movie.original_language] || "UN"} height="16" />
-							</span>
-							<p>
-								<strong>Voto:</strong> {movie.vote_average}
-								<span>
-									{/* Stelle piene */}
-									{Array.from({ length: stelle }, (_, i) => (
-										<span key={i} style={{ color: "#FFD700" }}>★</span>
-									))}
-									{/* Stelle vuote */}
-									{Array.from({ length: 5 - stelle }, (_, i) => (
-										<span key={i} style={{ color: "#ccc" }}>☆</span>
-									))}
-								</span>
-							</p>
-						</li>
-					);
-				})}
-			</ul>
+							);
+						})}
+					</div>
+				</div>
+			</section>
 		</>
 	)
 }
