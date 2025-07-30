ee5c8ce09d0efafd58398661d620dd7d26d056bc
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-31 00:48:06 +0200

    Aggiunta della gestione delle serie TV nel componente ListaFilm: aggiornamento della logica per visualizzare sia i film che le serie TV utilizzando il componente MovieCard.

--- src/pages/ListaFilm.jsx
+++ src/pages/ListaFilm.jsx
@@ -4,7 +4,7 @@ import { useGlobalContext } from "../contexts/GlobalContext";
 import MovieCard from "../components/Card";
 
 export default function ListaFilm() {
-	const { movies, loading, searchMovies, query, setQuery } = useGlobalContext();
+	const { movies, series, loading, searchMovies, query, setQuery } = useGlobalContext();
 
 	return (
 		<>
@@ -12,9 +12,19 @@ export default function ListaFilm() {
 
 			<main>
 				<div className="movie-grid">
-					{movies && movies.map(movie => (
-						<MovieCard key={movie.id} movie={movie} />
-					))}
+
+					<div >
+						<h2>Film</h2>
+						{movies && movies.map(movie => (
+							<MovieCard key={movie.id} movie={movie} />
+						))}
+					</div>
+					<div >
+						<h2>Serie TV</h2>
+						{series && series.map(serie => (
+							<MovieCard key={serie.id} movie={serie} />
+						))}
+					</div>
 				</div>
 			</main>
 
